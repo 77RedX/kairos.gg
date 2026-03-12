2e4faff9e75f6feecfa68536b5612e58e46e20c7
77RedX <akshaygautam4450@gmail.com>
2026-03-12 23:04:43 +0530

    Linux RAM write support

--- queuemgr/playback.py
+++ queuemgr/playback.py
@@ -1,14 +1,21 @@
 import yt_dlp
 import asyncio
+import os
+
+if os.name == 'posix':
+    _tmpl = "/dev/shm/kairos_%(id)s.%(ext)s"
+else:
+    _tmpl = "downloads/%(id)s.%(ext)s"
 
 YDL_OPTIONS = {
     "format": "bestaudio/best",
     "noplaylist": True,
-    "outtmpl": "downloads/%(id)s.%(ext)s",
+    "outtmpl": _tmpl,
     "quiet": True,
-    "js_runtimes": {
-        "node": {}
-    }
+    "js_runtimes": {"node": {}},
+    "concurrent_fragment_downloads": 10,
+    "http_chunk_size": 10485760,
+    "source_address": "0.0.0.0",
 }
 
 async def download_track(video_url):
